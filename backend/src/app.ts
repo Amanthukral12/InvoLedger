15caab2a27ea26b26298947463754141b747b8b9
Aman Thukral <thukral618@gmail.com>
2025-03-22 00:59:30 +0530

    Add util code for handling Api error and response along with asynHandler to handle the requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import passport from "./config/passport";
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("API is running");
