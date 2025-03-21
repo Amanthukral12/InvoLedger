57a033cd4a7f117e83126594e0b1cd119dd1e8c6
Aman Thukral <thukral618@gmail.com>
2025-03-22 00:44:39 +0530

    Add passportjs config for google auth

---
+++ backend/src/config/passport.ts
@@ -0,0 +1,71 @@
+import { PrismaClient } from "@prisma/client";
+import { Strategy as GoogleStrategy, Profile } from "passport-google-oauth20";
+import dotenv from "dotenv";
+import passport from "passport";
+import {
+  GoogleStrategyOptionsWithRequest,
+  VerifyCallbackDocument,
+} from "../types/types";
+import { v4 as uuidv4 } from "uuid";
+dotenv.config();
+const prisma = new PrismaClient();
+
+passport.serializeUser((session: any, done) => {
+  done(null, {
+    company: session.company,
+    sessionId: session.sessionId,
+  });
+});
+
+passport.deserializeUser(async (serializedSession: any, done) => {
+  try {
+    const company = await prisma.company.findUnique({
+      where: { id: serializedSession.company.id },
+    });
+    done(null, { company, sessionId: serializedSession.sessionId });
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENTID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+    } as GoogleStrategyOptionsWithRequest,
+    async (
+      req: any,
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done
+    ): Promise<void> => {
+      try {
+        let company = await prisma.company.findUnique({
+          where: { googleId: profile.id },
+        });
+        if (!company) {
+          company = await prisma.company.create({
+            data: {
+              googleId: profile.id,
+              email: profile.emails?.[0].value!,
+              name: profile.displayName,
+              avatar: profile.photos?.[0].value,
+            },
+          });
+        }
+        const sessionId = uuidv4();
+        const verifyCallbackDoc: VerifyCallbackDocument = {
+          company,
+          sessionId,
+        };
+        done(null, verifyCallbackDoc);
+      } catch (error) {
+        done(error, undefined, undefined);
+      }
+    }
+  )
+);
+export default passport;
---
+++ backend/src/types/types.ts
@@ -0,0 +1,58 @@
+export interface CompanyDocument {
+  id: number;
+  googleId: string;
+  email: string;
+  name: string;
+  avatar: string | null;
+  GST: string | null;
+  Address: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface SessionDocument {
+  id: string;
+  sessionId: string;
+  companyId: number;
+  refreshToken: string;
+  deviceInfo: string | null;
+  expiresAt: Date;
+  lastUsedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  company: CompanyDocument;
+}
+
+export interface CustomSession {
+  id: string;
+  sessionId: string;
+  companyId: number;
+  refreshToken: string;
+  deviceInfo: string | null;
+  expiresAt: Date;
+  lastUsedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface GoogleStrategyOptions {
+  clientID: string;
+  clientSecret: string;
+  callbackURL: string;
+}
+
+export interface GoogleStrategyOptionsWithRequest
+  extends GoogleStrategyOptions {
+  passReqToCallback: true;
+}
+
+export interface VerifyCallbackDocument {
+  company?: CompanyDocument;
+  sessionId: string;
+}
+
+export type VerifyCallback = (
+  error: any,
+  company?: VerifyCallbackDocument,
+  info?: any
+) => void;
