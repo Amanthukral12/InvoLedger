1d3d6a5be5302f5c5f999c2bedbda75fc7e3bef7
Aman Thukral <thukral618@gmail.com>
2025-04-22 01:45:58 +0530

    Add support PWA and add icon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,36 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  manifest: {
+    name: "InvoLedger",
+    short_name: "InvoLedger",
+    description: "An App to Manage and Generate GST invoices for the clients",
+    theme_color: "#000",
+    background_color: "#000",
+    display: "standalone",
+    scope: "/",
+    start_url: ".",
+    orientation: "portrait",
+    icons: [
+      {
+        src: "./assets/InvoLedger.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "./assets/InvoLedger.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+    ],
+  },
+};
 export default defineConfig({
   server: {
     proxy: {
@@ -13,5 +42,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), VitePWA(manifestForPlugin), tailwindcss(), svgr()],
 });
