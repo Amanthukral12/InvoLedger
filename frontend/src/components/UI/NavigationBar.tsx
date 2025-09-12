849dba8c3c0fe929e99a032d2bd51d348f1d5fc6
Aman Thukral <thukral618@gmail.com>
2025-09-13 01:27:29 +0530

    Refactor NavigationBar and Sidebar components to include Transactions link and improve layout structure

--- frontend/src/components/UI/NavigationBar.tsx
+++ frontend/src/components/UI/NavigationBar.tsx
@@ -6,86 +6,101 @@ import { FaHome } from "react-icons/fa";
 import { IoIosPeople } from "react-icons/io";
 import { IoMdPerson } from "react-icons/io";
 import { FaFileInvoice } from "react-icons/fa";
+import { GrTransaction } from "react-icons/gr";
 import { useState } from "react";
 import ProfilePopup from "./ProfilePopup";
 const NavigationBar = () => {
   const { company } = useAuthStore();
   const [showPopup, setShowPopup] = useState(false);
   return (
-    <div className="hidden bg-[#ffffff] m-3 min-h-[97vh] p-2.5 rounded-2xl lg:flex lg:flex-col items-center z-10 shadow-2xl text-main">
+    <div className="hidden bg-[#ffffff] m-3 p-2.5 h-screen rounded-2xl lg:flex lg:flex-col items-center z-10 shadow-2xl text-main">
       <div className="flex flex-col justify-center items-center my-7">
         <Logo className="w-1/2 rounded-3xl " />
         <h1 className="text-2xl font-bold text-main">InvoLedger</h1>
       </div>
       <hr className="w-[90%] border-t-2 border-main mb-4" />
-      <NavLink
-        className={({ isActive }) =>
-          `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-            isActive ? "bg-main !text-white rounded-2xl !shadow-lg" : ""
-          }`
-        }
-        to="/"
-      >
-        <FaHome className="h-8 w-8 mr-2.5" />
-        Home
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-            isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-          }`
-        }
-        to="/companyInvoices"
-      >
-        <FaFileInvoice className="h-8 w-8 mr-2.5" />
-        Invoices
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-            isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-          }`
-        }
-        to="/companyPurchases"
-      >
-        <FaFileInvoice className="h-8 w-8 mr-2.5" />
-        Purchases
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-            isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-          }`
-        }
-        to="/companyClients"
-      >
-        <IoIosPeople className="h-8 w-8 mr-2.5" />
-        Clients
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-            isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-          }`
-        }
-        to="/companyTransporters"
-      >
-        <IoIosPeople className="h-8 w-8 mr-2.5" />
-        Transporters
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-            isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-          }`
-        }
-        to="/profile"
-      >
-        <IoMdPerson className="h-8 w-8 mr-2.5" />
-        Profile
-      </NavLink>
+      <div className="flex-1 w-full overflow-y-auto pr-1">
+        <NavLink
+          className={({ isActive }) =>
+            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+              isActive ? "bg-main !text-white rounded-2xl !shadow-lg" : ""
+            }`
+          }
+          to="/"
+        >
+          <FaHome className="h-8 w-8 mr-2.5" />
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+            }`
+          }
+          to="/companyInvoices"
+        >
+          <FaFileInvoice className="h-8 w-8 mr-2.5" />
+          Invoices
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+            }`
+          }
+          to="/companyPurchases"
+        >
+          <FaFileInvoice className="h-8 w-8 mr-2.5" />
+          Purchases
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+            }`
+          }
+          to="/companyTransactions"
+        >
+          <GrTransaction className="h-8 w-8 mr-2.5" />
+          Transactions
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+            }`
+          }
+          to="/companyClients"
+        >
+          <IoIosPeople className="h-8 w-8 mr-2.5" />
+          Clients
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+            }`
+          }
+          to="/companyTransporters"
+        >
+          <IoIosPeople className="h-8 w-8 mr-2.5" />
+          Transporters
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+            }`
+          }
+          to="/profile"
+        >
+          <IoMdPerson className="h-8 w-8 mr-2.5" />
+          Profile
+        </NavLink>
+      </div>
+
       <div
-        className="flex items-center mt-auto mr-auto mb-4"
+        className="flex items-center mt-auto mr-auto mb-4 cursor-pointer"
         onClick={() => setShowPopup(!showPopup)}
       >
         <img
--- frontend/src/components/UI/Sidebar.tsx
+++ frontend/src/components/UI/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaFileInvoice, FaHome } from "react-icons/fa";
 import { IoIosPeople, IoMdPerson } from "react-icons/io";
+import { GrTransaction } from "react-icons/gr";
 import Logo from "../../assets/involedger.svg?react";
 import useAuthStore from "../../store/authStore";
 import ProfilePopup from "./ProfilePopup";
@@ -14,7 +15,7 @@ const Sidebar = ({ shown, close }: { shown: boolean; close: () => void }) => {
       onClick={() => close()}
     >
       <div
-        className="bg-[#D9D9D9] absolute top-0 left-0 w-3/5 min-h-screen p-2 rounded-lg flex flex-col items-center backdrop:blur-sm text-main"
+        className="bg-[#D9D9D9] absolute top-0 left-0 w-3/5 h-screen p-2 rounded-lg flex flex-col items-center backdrop:blur-sm text-main"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex flex-col justify-center items-center my-7">
@@ -24,72 +25,86 @@ const Sidebar = ({ shown, close }: { shown: boolean; close: () => void }) => {
           </h1>
         </div>
         <hr className="w-[90%] border-t-2 border-main mb-4" />
-        <NavLink
-          className={({ isActive }) =>
-            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-              isActive ? "bg-main !text-white rounded-2xl !shadow-lg" : ""
-            }`
-          }
-          to="/"
-        >
-          <FaHome className="h-8 w-8 mr-2.5" />
-          Home
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-            }`
-          }
-          to="/companyInvoices"
-        >
-          <FaFileInvoice className="h-8 w-8 mr-2.5" />
-          Invoices
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-            }`
-          }
-          to="/companyPurchases"
-        >
-          <FaFileInvoice className="h-8 w-8 mr-2.5" />
-          Purchases
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-            }`
-          }
-          to="/companyClients"
-        >
-          <IoIosPeople className="h-8 w-8 mr-2.5" />
-          Clients
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-            }`
-          }
-          to="/companyTransporters"
-        >
-          <IoIosPeople className="h-8 w-8 mr-2.5" />
-          Transporters
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
-              isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
-            }`
-          }
-          to="/profile"
-        >
-          <IoMdPerson className="h-8 w-8 mr-2.5" />
-          Profile
-        </NavLink>
+        <div className="w-[90%] overflow-y-auto">
+          <NavLink
+            className={({ isActive }) =>
+              `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+                isActive ? "bg-main !text-white rounded-2xl !shadow-lg" : ""
+              }`
+            }
+            to="/"
+          >
+            <FaHome className="h-8 w-8 mr-2.5" />
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+                isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+              }`
+            }
+            to="/companyInvoices"
+          >
+            <FaFileInvoice className="h-8 w-8 mr-2.5" />
+            Invoices
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+                isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+              }`
+            }
+            to="/companyPurchases"
+          >
+            <FaFileInvoice className="h-8 w-8 mr-2.5" />
+            Purchases
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+                isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+              }`
+            }
+            to="/companyTransactions"
+          >
+            <GrTransaction className="h-8 w-8 mr-2.5" />
+            Transactions
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+                isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+              }`
+            }
+            to="/companyClients"
+          >
+            <IoIosPeople className="h-8 w-8 mr-2.5" />
+            Clients
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+                isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+              }`
+            }
+            to="/companyTransporters"
+          >
+            <IoIosPeople className="h-8 w-8 mr-2.5" />
+            Transporters
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              `p-2.5 w-[90%] text-lg  font-bold flex items-center mb-1 ${
+                isActive ? "bg-main !text-white rounded-2xl !shadow-2xl" : ""
+              }`
+            }
+            to="/profile"
+          >
+            <IoMdPerson className="h-8 w-8 mr-2.5" />
+            Profile
+          </NavLink>
+        </div>
         <div
           className="flex items-center mt-auto mr-auto mb-4"
           onClick={() => setShowPopup(!showPopup)}
