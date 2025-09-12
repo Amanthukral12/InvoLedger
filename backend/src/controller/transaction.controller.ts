214c5103922e9a80cfaf0322d639d7eb6e9c4f64
Aman Thukral <thukral618@gmail.com>
2025-09-13 01:18:26 +0530

    Fix transactions schema, add getAllTransactionsForCompanyForMonth, getAllTransactionsForCompanyForMonthGroupedByClient controller functions, fix createTransaction function

--- backend/src/controller/transaction.controller.ts
+++ backend/src/controller/transaction.controller.ts
@@ -12,9 +12,12 @@ export const createTransaction = asyncHandler(
       ]);
     }
     const companyId = req.company.id;
-    const clientId = req.params.clientId;
+    const clientIdFromParams = req.params.clientId;
 
-    const { amount, type, date, description } = req.body;
+    const { amount, type, date, description, bankName, clientIdFromBody } =
+      req.body;
+
+    const clientId = clientIdFromBody || clientIdFromParams;
 
     if (!clientId || !amount || !type || !date) {
       throw new ApiError(400, "Please fill all the fields", [
@@ -22,15 +25,18 @@ export const createTransaction = asyncHandler(
       ]);
     }
 
+    const cleanedDescription = description !== null ? description : "";
+
     const transaction = await prisma.$transaction(async (prisma) => {
       const newTransaction = await prisma.transaction.create({
         data: {
           companyId,
           clientId,
           amount,
+          bankName,
           type,
           date,
-          description,
+          description: cleanedDescription,
         },
       });
 
@@ -132,5 +138,95 @@ export const getAllTransactionsForClient = asyncHandler(
       .json(
         new ApiResponse(200, transactions, "Transactions fetched successfully")
       );
+  }
+);
+
+export const getAllTransactionsForCompanyForMonth = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.company) {
+      throw new ApiError(401, "Unauthorized Access. Please login again", [
+        "Unauthorized Access. Please login again",
+      ]);
+    }
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    const companyId = req.company.id;
+    const { year = currentYear, month = currentMonth } = req.query;
+    const startDate = new Date(Number(year), Number(month) - 1, 1);
+    const endDate = new Date(Number(year), Number(month), 1);
+
+    const transactions = await prisma.transaction.findMany({
+      where: {
+        companyId: Number(companyId),
+        date: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+    });
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, transactions, "Transactions fetched successfully")
+      );
+  }
+);
+
+export const getAllTransactionsForCompanyForMonthGroupedByClient = asyncHandler(
+  async (req: Request, res: Response) => {
+    if (!req.company) {
+      throw new ApiError(401, "Unauthorized Access. Please login again", [
+        "Unauthorized Access. Please login again",
+      ]);
+    }
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    const companyId = req.company.id;
+    const { year = currentYear, month = currentMonth } = req.query;
+    const startDate = new Date(Number(year), Number(month) - 1, 1);
+    const endDate = new Date(Number(year), Number(month), 1);
+
+    const transactions = await prisma.$queryRaw<
+      {
+        id: number;
+        amount: number;
+        date: Date;
+        clientId: string;
+        clientName: string;
+      }[]
+    >`
+    SELECT t.id, t.amount, t.date, t.type, COALESCE(t.description, '') as description, t."clientId", c."name" AS "clientName"
+    FROM "Transaction" t
+    JOIN "Client" c on t."clientId" = c.id
+    where t."companyId" = ${Number(companyId)} AND t.date >= ${startDate} AND t.date < ${endDate}
+    ORDER BY c."name", t.date;
+    `;
+
+    const totals = await prisma.$queryRaw<
+      { clientId: string; clientName: string; totalAmount: number }[]
+    >`
+    SELECT t."clientId", c."name" AS "clientName", SUM(
+      CASE
+        WHEN t.type = 'CREDIT' THEN t.amount
+        WHEN t.type = 'DEBIT' THEN -t.amount
+        ELSE 0
+      END
+    ) AS "totalAmount"
+    FROM "Transaction" t
+    JOIN "Client" c on t."clientId" = c.id
+    where t."companyId" = ${Number(companyId)}
+    AND t.date >= ${startDate} AND t.date < ${endDate}
+    GROUP BY t."clientId", c."name"
+    ORDER BY c."name";
+    `;
+
+    const grouped = totals.map((t) => ({
+      clientName: t.clientName,
+      total: Number(t.totalAmount),
+      transactions: transactions.filter((tr) => tr.clientId === t.clientId),
+    }));
+    return res
+      .status(200)
+      .json(new ApiResponse(200, grouped, "Transactions fetched successfully"));
   }
 );
--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ log: ["query"] });
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
